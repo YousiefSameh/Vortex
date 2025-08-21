54556cfe7b4319874af629e0b78f57332f014fc6
Yousief Sameh <yousief.sameh@outlook.com>
2025-08-21 11:39:53 +0300

    Make Header and Footer: - Make a responsive header and animated with framer motion
    - Make an animated, simple footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,12 @@
+import Logo from "@/assets/logo.png";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-[#260932] h-[100px] w-full flex flex-col items-center justify-center text-white fixed bottom-0 left-0 right-0 z-50">
+      <img src={Logo} alt="Vortex Logo" />
+      <p className="text-sm font-semibold">© 2025 Vortex. All rights reserved.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
